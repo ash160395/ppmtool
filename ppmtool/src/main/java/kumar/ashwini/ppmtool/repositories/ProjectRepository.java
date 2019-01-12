925b087582dbd6abfbf3d5949d7e090bcf89f618
Ashwini Kumar <ashwini.kumar957@gmail.com>
2019-01-13 02:36:34 +0530

    Added Project Class and Repository

---
+++ ppmtool/src/main/java/kumar/ashwini/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,8 @@
+package kumar.ashwini.ppmtool.repositories;
+
+import kumar.ashwini.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
